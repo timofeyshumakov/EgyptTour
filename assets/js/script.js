27e1eb67e4b5c7232983a22cea260689ab7cc976
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2024-03-12 18:39:36 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
-function openGoogleMaps() {
-  var coordinates = "@30.0763082,31.2648382,16z?";
-  if (navigator.platform.indexOf("iPhone") != -1 || navigator.platform.indexOf("iPad") != -1 || navigator.platform.indexOf("iPod") != -1) {
-    window.open("maps://maps.google.com/maps?"+coordinates); // Открыть в приложении Google Карты на iOS
-  } else if (navigator.platform.indexOf("Android") != -1) {
-    window.open("geo:"+coordinates); // Открыть в приложении Google Карты на Android
-  } else {
-    window.open("https://maps.google.com/maps?"+coordinates); // Открыть в веб-браузере
-  }
-}
+function openGoogleMaps(){var o="@30.0763082,31.2648382,16z?";-1!=navigator.platform.indexOf("iPhone")||-1!=navigator.platform.indexOf("iPad")||-1!=navigator.platform.indexOf("iPod")?window.open("maps://maps.google.com/maps?"+o):-1!=navigator.platform.indexOf("Android")?window.open("geo:"+o):window.open("https://maps.google.com/maps?"+o)}
